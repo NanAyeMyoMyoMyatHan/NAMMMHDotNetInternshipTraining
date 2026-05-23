f963bc5f50ba847502c83733277d1943270fd972
NanAyeMyoMyoMyatHan <nanayemyomyomyathan@gmail.com>
2026-05-23 16:23:12 +0630

    feat: test connection

---
+++ NAMMMHDotNetInternshipTraining.EFCoreSample2/EFCoreSample.cs
@@ -0,0 +1,91 @@
+﻿using NAMMMHDotNetInternshipTraining.EFCoreSample2.DataBaseFirst.AppDbModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAMMMHDotNetInternshipTraining.EFCoreSample2;
+
+public class EFCoreSample
+{
+    private readonly AppDbContext _context;
+    public EFCoreSample()
+    {
+        _context = new AppDbContext();
+    }
+    public void ReadStudents()
+    {
+        List<TblStudent> students = _context.TblStudents.ToList();
+        foreach (TblStudent student in students)
+        {
+            Console.WriteLine($"Student No: {student.StudentNo}, Student Name: {student.StudentName}");
+        }
+    }
+    public void CreateStudent()
+    {
+        var student = new TblStudent
+        {
+            StudentNo = "S001",
+            StudentName = "John Doe",
+            FatherName = "Richard Roe",
+            Address = "123 Main St",
+            DateOfBirth = new DateTime(2000, 1, 1),
+            IsDelete = false,
+            CreatedDateTime = DateTime.Now,
+            CreatedBy = "Admin",
+            ModifiedDateTime = DateTime.Now,
+            ModifiedBy = "Admin"
+
+        };
+        _context.TblStudents.Add(student);
+        int result = _context.SaveChanges();
+        Console.WriteLine(result > 0 ? "Seaving Successful." : "Saving Failed");
+    }
+    public void UpdateStudent()
+    {
+        var student = _context.TblStudents.FirstOrDefault(s => s.StudentId == 1);
+        if (student != null)
+        {
+            student.Address = "456 Elm St";
+            student.ModifiedDateTime = DateTime.Now;
+            student.ModifiedBy = "Admin";
+            int result = _context.SaveChanges();
+            Console.WriteLine(result > 0 ? "Update Successful." : "Update Failed");
+        }
+        else
+        {
+            Console.WriteLine("Student not found.");
+        }
+    }
+    public void DeleteStudent()
+    {
+        var student = _context.TblStudents.FirstOrDefault(s => s.StudentId == 1);
+        if (student != null)
+        {
+            student.IsDelete = true;
+            student.ModifiedDateTime = DateTime.Now;
+            student.ModifiedBy = "Admin";
+            int result = _context.SaveChanges();
+            Console.WriteLine(result > 0 ? "Delete Successful." : "Delete Failed");
+        }
+        else
+        {
+            Console.WriteLine("Student not found.");
+        }
+    }
+    public void EditSutdent()
+    {
+        var student = _context.TblStudents.FirstOrDefault(s => s.StudentId == 1);
+        if (student != null)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(student));
+            Console.WriteLine(JsonConvert.SerializeObject(student, Formatting.Indented));
+        }
+        else
+        {
+            Console.WriteLine("Student not found.");
+        }
+    }
+}
---
+++ NAMMMHDotNetInternshipTraining.EFCoreSample2/Program.cs
@@ -0,0 +1,12 @@
+﻿using NAMMMHDotNetInternshipTraining.EFCoreSample2;
+
+EFCoreSample efcore = new EFCoreSample();
+efcore.ReadStudents();
+efcore.EditSutdent();
+efcore.CreateStudent();
+efcore.ReadStudents();
+efcore.UpdateStudent();
+efcore.ReadStudents();
+efcore.DeleteStudent();
+efcore.ReadStudents();
+Console.ReadLine();
